8e14ef9b08d74f8e1258bc5ca98aee9048080414
guragaidipesh <guragaindipesh89@gmail.com>
2026-03-03 00:07:56 +0545

    Add note_list view

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Note
 
-# Create your views here.
+def note_list(request):
+    notes = Note.objects.all().order_by('-created_at')
+    return render(request, 'notes/note_list.html', {'notes': notes})
+
+
+def note_detail(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    return render(request, 'notes/note_detail.html', {'note': note})
